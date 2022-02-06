2d9cc9eb033c5120e07a581e2879635bd4d8ff1b
musadhiq mp <musadhiqmp@gmail.com>
2022-02-06 17:58:36 +0530

    download page set

--- src/components/download/Download.jsx
+++ src/components/download/Download.jsx
@@ -1,14 +1,41 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdDownload } from "react-icons/md";
-import data from "../../assets/data.json";
+import wAdata from "../../assets/wa.json";
+import instadata from "../../assets/insta.json";
 
 function Download() {
-  console.log(data);
+  const [active, setActive] = useState(true);
+  let data;
+  if (active) {
+    data = wAdata;
+  } else {
+    data = instadata;
+  }
   return (
     <div className="download">
       <h1 className="head">
         Downloads from <span className="highlight-white">nyz</span>apps
       </h1>
+      <div className="tabs">
+        <div
+          className={active ? "tab-wa active" : "tab-wa"}
+          onClick={() => setActive(true)}
+        >
+          <div className="tab-head">
+            <h4>nyzwhatsapp</h4>
+          </div>
+          <div className="whatsapp"></div>
+        </div>
+        <div
+          className={active ? "tab-insta" : "tab-insta active"}
+          onClick={() => setActive(false)}
+        >
+          <div className="tab-head">
+            <h4>nyzinsta</h4>
+          </div>
+          <div className="instagram"></div>
+        </div>
+      </div>
       <div className="download-items">
         <ul className="datas">
           {data.map((item, index) => (
